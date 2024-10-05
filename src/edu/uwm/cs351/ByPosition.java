4bb88877d43fdeb0f708914a866ca8b97ff9dd97
siddhi2218 <siddhi2218@mac.lan>
2024-10-04 22:35:10 -0500

    Implemented the ByPosition class succesfully hence committing

--- src/edu/uwm/cs351/ByPosition.java
+++ src/edu/uwm/cs351/ByPosition.java
@@ -13,7 +13,21 @@ public class ByPosition implements Comparator<Player> {
 	public int compare(Player o1, Player o2) {
 		// TODO:  Hint the "ordinal" method is useful here.
 		// but remember that earlier is higher priority
-		return 0;
+		if (o1.getPosition() == null && o2.getPosition() == null) {
+            return 0;  // Both are null, so they are considered equal in priority
+        }
+		
+		 if (o2.getPosition() == null) {
+	         return -1;   // o2 is nulL meaning it has higher priority
+
+	        }
+		 
+		 if (o1.getPosition() == null) {
+            return 1;  // o1 is null meaning it has higher priority
+        }
+       
+        return Integer.compare(o2.getPosition().ordinal(), o1.getPosition().ordinal());
+	
 	}
 
 	@Override //implementation
